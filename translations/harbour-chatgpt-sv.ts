b38bd5940e65d34c48eef9462b77f07ae010aba0
Åke Engelbrektson <eson57@users.noreply.github.com>
2023-03-19 15:00:32 +0100

    Create harbour-chatgpt-sv.ts
    Add Swedish translation

---
+++ translations/harbour-chatgpt-sv.ts
@@ -0,0 +1,165 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>ApiKeyHelpPage</name>
+    <message>
+        <source>Api key help</source>
+        <extracomment>Page title</extracomment>
+        <translation>Hjälp med API-nyckel</translation>
+    </message>
+    <message>
+        <source>To get an api key, first go to &lt;a href=&apos;%1&apos;&gt;%1&lt;/a&gt; and create an account or log in to your existing one.</source>
+        <translation>För att få en API-nyckel skapar du först ett konto eller loggar in på ett befintligt, på &lt;a href=&apos;%1&apos;&gt;%1&lt;/a&gt;.</translation>
+    </message>
+    <message>
+        <source>Afterwards click the &lt;strong&gt;Create new secret key&lt;/strong&gt; button and copy it from the resulting text box. The key won&apos;t be shown again so make sure you have copied it.</source>
+        <translation>Där klickar du på &lt;strong&gt;Create new secret key&lt;/strong&gt; och kopierar nyckeln från resulterande textruta. Nyckeln kommer inte att visas igen, så tillse att du verkligen kopierar den.</translation>
+    </message>
+    <message>
+        <source>You can now go to the previous page in this application and paste the api key into the api key field.</source>
+        <translation>Du kan nu gå till föregående sida i denna app och klistra in nyckeln i API-nyckelfältet.</translation>
+    </message>
+</context>
+<context>
+    <name>ChangeChatTitleDialog</name>
+    <message>
+        <source>Change title</source>
+        <extracomment>Dialog accept text</extracomment>
+        <translation>Ändra titel</translation>
+    </message>
+    <message>
+        <source>New title</source>
+        <translation>Ny titel</translation>
+    </message>
+</context>
+<context>
+    <name>Chat</name>
+    <message>
+        <source>Unnamed chat</source>
+        <translation>Namnlös chatt</translation>
+    </message>
+</context>
+<context>
+    <name>ChatDetailPage</name>
+    <message>
+        <source>Chat</source>
+        <extracomment>Page title</extracomment>
+        <translation>Chatt</translation>
+    </message>
+    <message>
+        <source>Change title</source>
+        <translation>Ändra titel</translation>
+    </message>
+    <message>
+        <source>Type your messsage</source>
+        <translation>Skriv ditt meddelande</translation>
+    </message>
+</context>
+<context>
+    <name>ChatGptClient</name>
+    <message>
+        <source>There was a network error.</source>
+        <translation>Ett nätverksfel inträffade.</translation>
+    </message>
+    <message>
+        <source>ChatGPT provided no response.</source>
+        <translation>ChatGPT svarade inte.</translation>
+    </message>
+</context>
+<context>
+    <name>ChatListPage</name>
+    <message>
+        <source>Chat list</source>
+        <extracomment>Page title</extracomment>
+        <translation>Chattlista</translation>
+    </message>
+    <message>
+        <source>New chat</source>
+        <translation>Ny chatt</translation>
+    </message>
+    <message>
+        <source>There are no chats, use pull down menu to create one.</source>
+        <translation>Det finns inga chattar, använd toppmenyn för att skapa en.</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Ta bort</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+</context>
+<context>
+    <name>ConfirmSettingDialog</name>
+    <message>
+        <source>Confirm</source>
+        <extracomment>Dialog accept text</extracomment>
+        <translation>Bekräfta</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>ChatGPT</source>
+        <translation>ChatGPT</translation>
+    </message>
+</context>
+<context>
+    <name>InitialChecksPage</name>
+    <message>
+        <source>Initializing...</source>
+        <extracomment>Page title</extracomment>
+        <translation>Startar...</translation>
+    </message>
+    <message>
+        <source>The provided api key is invalid.</source>
+        <translation>Tillhandahållen API-nyckel är ogiltig.</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <source>Save api key</source>
+        <extracomment>Dialog accept text</extracomment>
+        <translation>Spara API-nyckeln</translation>
+    </message>
+    <message>
+        <source>Api key</source>
+        <translation>API-nyckel</translation>
+    </message>
+    <message>
+        <source>To use this app you need to provide your own ChatGPT api key. &lt;a href=&apos;#help&apos; style=&apos;color: %1;&apos;&gt;Click here&lt;/a&gt; to get help with getting the api key.</source>
+        <translation>Fär att använda den här appen måste du ange en egen API-nyckel från ChatGPT. &lt;a href=&apos;#help&apos; style=&apos;color: %1;&apos;&gt;Tryck här&lt;/a&gt; för hjälp med att skaffa en API-nyckel.</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <extracomment>Page title</extracomment>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>Configure the length of the conversation that is sent to the ChatGPT model.</source>
+        <translation>Ange längden på konversationen som skickas till ChatGPT-modellen.</translation>
+    </message>
+    <message>
+        <source>To maintain context, the AI model needs to see the previous messages as well, this configures how many past messages are sent.</source>
+        <translation>För att bibehålla kontext måste AI-modellen se tidigare meddelanden också. Detta anger hur många tidigare meddelanden som skickas.</translation>
+    </message>
+    <message>
+        <source>Note that previous messages count towards the token limit of the AI model which is currently %1 tokens.</source>
+        <translation>Observera att tidigare meddelanden räknas in i tokengränsen för AI-modellen som för närvarande är %1 token.</translation>
+    </message>
+    <message>
+        <source>Conversation length (number of messages)</source>
+        <translation>Konversationslängd (antal meddelanden)</translation>
+    </message>
+    <message>
+        <source>Conversation length: %1</source>
+        <translation>Konversationslängd: %1</translation>
+    </message>
+</context>
+</TS>
