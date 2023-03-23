9b881c0a5efa4879f117a4408a9a272db6b4e740
Dominik Chrástecký <dominik@chrastecky.cz>
2023-03-23 16:52:14 +0100

    Fix bug with incorrectly displaying numbers (#9)

--- translations/harbour-chatgpt-sv.ts
+++ translations/harbour-chatgpt-sv.ts
@@ -1,4 +1,6 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="sv">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
 <context>
     <name>ApiKeyHelpPage</name>
     <message>
@@ -293,7 +295,7 @@ Read the description at https://platform.openai.com/docs/api-reference/chat/crea
     </message>
     <message>
         <source>Note: The system message counts towards your &apos;%1&apos; limit.</source>
-        <extracomment>The %1 gets replaced by the translation for 'Conversation length'</extracomment>
+        <extracomment>The %1 gets replaced by the translation for &apos;Conversation length&apos;</extracomment>
         <translation>Notera att systemmeddelandet räknas in i din gräns på &quot;%1&quot; meddelanden.</translation>
     </message>
     <message>
