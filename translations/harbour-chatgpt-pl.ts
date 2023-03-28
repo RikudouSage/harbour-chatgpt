b3e488d6ba15e4bad0d19c0d63e8df2df354ade0
PiotrD <likot180@gmail.com>
2023-03-28 15:30:54 +0200

    Create harbour-chatgpt-pl.ts (#17)

---
+++ translations/harbour-chatgpt-pl.ts
@@ -0,0 +1,312 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="pl">
+<context>
+    <name>ApiKeyHelpPage</name>
+    <message>
+        <source>Api key help</source>
+        <extracomment>Page title</extracomment>
+        <translation>Pomoc z kluczem API</translation>
+    </message>
+    <message>
+        <source>To get an api key, first go to &lt;a href=&apos;%1&apos;&gt;%1&lt;/a&gt; and create an account or log in to your existing one.</source>
+        <translation>Aby uzyskać klucz API, wejdź na stronę &lt;a href=&apos;%1&apos;&gt;%1&lt;/a&gt; i utwórz konto lub zaloguj się do istniejącego.</translation>
+    </message>
+    <message>
+        <source>Afterwards click the &lt;strong&gt;Create new secret key&lt;/strong&gt; button and copy it from the resulting text box. The key won&apos;t be shown again so make sure you have copied it.</source>
+        <translation>Następnie kliknij przycisk &lt;strong&gt;Create new secret key&lt;/strong&gt; i skopiuj otrzymany klucz. Klucz nie pojawi się ponownie, więc upewnij się, że faktycznie go skopiowałeś.</translation>
+    </message>
+    <message>
+        <source>You can now go to the previous page in this application and paste the api key into the api key field.</source>
+        <translation>Po skopiowaniu klucza możesz przejść do poprzedniej strony tej aplikacji i wkleić go w polu.</translation>
+    </message>
+</context>
+<context>
+    <name>ChangeChatTitleDialog</name>
+    <message>
+        <source>Change title</source>
+        <extracomment>Dialog accept text</extracomment>
+        <translation>Zmień nazwę</translation>
+    </message>
+    <message>
+        <source>New title</source>
+        <translation>Nowa nazwa</translation>
+    </message>
+</context>
+<context>
+    <name>Chat</name>
+    <message>
+        <source>Unnamed chat</source>
+        <translation>Czat bez nazwy</translation>
+    </message>
+</context>
+<context>
+    <name>ChatDetailPage</name>
+    <message>
+        <source>Chat</source>
+        <extracomment>Page title</extracomment>
+        <translation>Czat</translation>
+    </message>
+    <message>
+        <source>Change title</source>
+        <translation>Zmień nazwę</translation>
+    </message>
+    <message>
+        <source>Type your message</source>
+        <translation>Napisz wiadomość</translation>
+    </message>
+</context>
+<context>
+    <name>ChatListPage</name>
+    <message>
+        <source>Chat list</source>
+        <extracomment>Page title</extracomment>
+        <translation>Lista czatów</translation>
+    </message>
+    <message>
+        <source>New chat</source>
+        <translation>Nowy czat</translation>
+    </message>
+    <message>
+        <source>There are no chats, use pull down menu to create one.</source>
+        <translation>Brak czatów, użyj górnego menu, aby je utworzyć.</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Usuń</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Ustawienia</translation>
+    </message>
+    <message>
+        <source>Logs</source>
+        <translation>Logi</translation>
+    </message>
+    <message>
+        <source>Rename</source>
+        <translation>Zmień nazwę</translation>
+    </message>
+</context>
+<context>
+    <name>ConfirmSettingDialog</name>
+    <message>
+        <source>Confirm</source>
+        <extracomment>Dialog accept text</extracomment>
+        <translation>Potwierdź</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>ChatGPT</source>
+        <translation>ChatGPT</translation>
+    </message>
+</context>
+<context>
+    <name>InitialChecksPage</name>
+    <message>
+        <source>Initializing...</source>
+        <extracomment>Page title</extracomment>
+        <translation>Inicjowanie...</translation>
+    </message>
+    <message>
+        <source>The provided api key is invalid.</source>
+        <translation>Podany klucz API nie jest poprawny.</translation>
+    </message>
+</context>
+<context>
+    <name>LoginPage</name>
+    <message>
+        <source>Save api key</source>
+        <extracomment>Dialog accept text</extracomment>
+        <translation>Zapisz klucz API</translation>
+    </message>
+    <message>
+        <source>Api key</source>
+        <translation>Klucz API</translation>
+    </message>
+    <message>
+        <source>To use this app you need to provide your own ChatGPT api key. &lt;a href=&apos;#help&apos; style=&apos;color: %1;&apos;&gt;Click here&lt;/a&gt; to get help with getting the api key.</source>
+        <translation>Aby korzystać z tej aplikacji, musisz wprowadzić własny klucz API ChatGPT. &lt;a href=&apos;#help&apos; style=&apos;color: %1;&apos;&gt;Kliknij tu&lt;/a&gt;, aby uzyskać pomoc w celu uzyskania klucza API.</translation>
+    </message>
+</context>
+<context>
+    <name>LogsPage</name>
+    <message>
+        <source>Logs</source>
+        <extracomment>Page title</extracomment>
+        <translation>Logi</translation>
+    </message>
+    <message>
+        <source>Delete logs</source>
+        <translation>Usuń logi</translation>
+    </message>
+    <message>
+        <source>Export to file</source>
+        <translation>Eksportuj do pliku</translation>
+    </message>
+    <message>
+        <source>There are no logs.</source>
+        <translation>Brak logów.</translation>
+    </message>
+    <message>
+        <source>There was an error when trying to open the export file &apos;%1&apos; in your Documents folder for writing.</source>
+        <translation>Wystąpił błąd podczas zapisywania eksportowanego pliku &quot;%1&quot; w folderze Dokumenty.</translation>
+    </message>
+    <message>
+        <source>Logs have been exported! You should find them in your Documents folder under the name &apos;%1&apos;.</source>
+        <translation>Logi zostały wyeksportowane! Możesz je znaleźć w folderze Dokumenty pod nazwą &quot;%1&quot;.</translation>
+    </message>
+    <message>
+        <source>Click &lt;a href=&apos;%2&apos;&gt;here&lt;/a&gt; to contact the developer via email.</source>
+        <translation>Kliknij &lt;a href=&apos;%2&apos;&gt;här&lt;/a&gt;, aby skontaktować się z deweloperem przez e-mail.</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Settings</source>
+        <extracomment>Page title</extracomment>
+        <translation>Ustawienia</translation>
+    </message>
+    <message>
+        <source>Configure the length of the conversation that is sent to the ChatGPT model.</source>
+        <translation>Wprowadź długość rozmowy wysyłanej do modelu ChatGPT.</translation>
+    </message>
+    <message>
+        <source>To maintain context, the AI model needs to see the previous messages as well, this configures how many past messages are sent.</source>
+        <translation>Aby zachować kontekst, model AI musi widzieć również poprzednie wiadomości. W tej opcji można skonfigurować liczbę wysłanych poprzednich wiadomości.</translation>
+    </message>
+    <message>
+        <source>Note that previous messages count towards the token limit of the AI model which is currently %1 tokens.</source>
+        <translation>Pamiętaj, że poprzednie wiadomości wliczają się do limitu tokenów modelu AI, który obecnie wynosi %1 tokenów.</translation>
+    </message>
+    <message>
+        <source>Conversation length (number of messages)</source>
+        <translation>Długość rozmowy (liczba wiadomości)</translation>
+    </message>
+    <message>
+        <source>Conversation length: %1</source>
+        <translation>Długość rozmowy: %1</translation>
+    </message>
+    <message>
+        <source>What sampling temperature to use.</source>
+        <translation>Wybór temperatury próbkowania.</translation>
+    </message>
+    <message>
+        <source>Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.</source>
+        <translation>Wyższe wartości, takie jak 0,8, sprawią, że wynik będzie bardziej losowy, podczas gdy niższe wartości, takie jak 0,2, sprawią, że będzie on bardziej skoncentrowany i deterministyczny.</translation>
+    </message>
+    <message>
+        <source>It&apos;s generally recommended to alter this setting or top percentage, but not both.</source>
+        <translation>Zwykle zaleca się zmianę tego ustawienia lub najwyższej wartości procentowej, ale nie obu jednocześnie.</translation>
+    </message>
+    <message>
+        <source>Sampling temperature: %1</source>
+        <extracomment>Read the description at https://platform.openai.com/docs/api-reference/chat/create#chat/create-temperature to understand what the temperature here means. I recommend keeping it untranslated if the translation is not used in the AI community in your language.</extracomment>
+        <translation>Temperatura próbkowania: %1</translation>
+    </message>
+    <message>
+        <source>Sampling temperature</source>
+        <extracomment>Read the description at https://platform.openai.com/docs/api-reference/chat/create#chat/create-temperature to understand what the temperature here means. I recommend keeping it untranslated if the translation is not used in the AI community in your language.</extracomment>
+        <translation>Temperatura próbkowania</translation>
+    </message>
+    <message>
+        <source>Top percentage: %1</source>
+        <extracomment>Read the description at https://platform.openai.com/docs/api-reference/chat/create#chat/create-top_p to understand what it means. I recommend keeping it untranslated if the translation is not used in the AI community in your language.</extracomment>
+        <translation>Najwyższy procent: %1</translation>
+    </message>
+    <message>
+        <source>An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top percentage probability mass.</source>
+        <translation>Alternatywa dla temperatury próbkowania, zwana próbkowaniem jądra, gdzie model uwzględnia wyniki tokena o najwyższej masie procentowej prawdopodobieństwa.</translation>
+    </message>
+    <message>
+        <source>So 0.1 means only the tokens comprising the top 10% probability mass are considered.</source>
+        <translation>Zatem 0,1 oznacza, że brane są pod uwagę tylko te tokeny, które składają się z 10% najwyższych mas prawdopodobieństwa.</translation>
+    </message>
+    <message>
+        <source>It&apos;s generally recommended to alter this setting or sampling temperature, but not both.</source>
+        <translation>Zwykle zaleca się zmianę tego ustawienia lub temperatury próbkowania, ale nie obu jednocześnie.</translation>
+    </message>
+    <message>
+        <source>Top percentage</source>
+        <extracomment>Read the description at https://platform.openai.com/docs/api-reference/chat/create#chat/create-top_p to understand what it means. I recommend keeping it untranslated if the translation is not used in the AI community in your language.</extracomment>
+        <translation>Najwyższy procent</translation>
+    </message>
+    <message>
+        <source>Presence penalty: %1</source>
+        <extracomment>Read the description at https://platform.openai.com/docs/api-reference/chat/create#chat/create-presence_penalty to understand what it means. I recommend keeping it untranslated if the translation is not used in the AI community in your language.</extracomment>
+        <translation>Kara za obecność słowa: %1</translation>
+    </message>
+    <message>
+        <source>Positive values penalize new tokens based on whether they appear in the text so far, increasing the model&apos;s likelihood to talk about new topics.</source>
+        <translation>Wartości dodatnie nakładają kary na nowe tokeny na podstawie tego, czy pojawiają się one w tekście do tej pory, zwiększając prawdopodobieństwo, że model będzie mówił o nowych tematach.</translation>
+    </message>
+    <message>
+        <source>Presence penalty</source>
+        <extracomment>Read the description at https://platform.openai.com/docs/api-reference/chat/create#chat/create-presence_penalty to understand what it means. I recommend keeping it untranslated if the translation is not used in the AI community in your language.
+----------
+Read the description at https://platform.openai.com/docs/api-reference/chat/create#chat/create-frequency_penalty to understand what it means. I recommend keeping it untranslated if the translation is not used in the AI community in your language.</extracomment>
+        <translation>Kara za obecność słowa</translation>
+    </message>
+    <message>
+        <source>Frequency penalty: %1</source>
+        <extracomment>Read the description at https://platform.openai.com/docs/api-reference/chat/create#chat/create-frequency_penalty to understand what it means. I recommend keeping it untranslated if the translation is not used in the AI community in your language.</extracomment>
+        <translation>Kara za częstotliwość występowania: %1</translation>
+    </message>
+    <message>
+        <source>Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model&apos;s likelihood to repeat the same line verbatim.</source>
+        <translation>Wartości dodatnie nakładają karę na nowe tokeny na podstawie ich dotychczasowej częstotliwości wystąpienia w tekście, zmniejszając prawdopodobieństwo powtórzenia przez model tego samego wiersza.</translation>
+    </message>
+    <message>
+        <source>Enable logging</source>
+        <translation>Włącz logowanie</translation>
+    </message>
+    <message>
+        <source>If you have problems with the app, enable this option to collect logs which you can then send to the developer.</source>
+        <translation>Jeśli masz problemy z aplikacją, włącz tę opcję, aby zbierać logi, które możesz wysłać do programisty.</translation>
+    </message>
+    <message>
+        <source>You need to restart the app for the logging to take full effect.</source>
+        <extracomment>Full effect here means that the setting will not propagate to all parts of the app so some parts will not collect logs until restart</extracomment>
+        <translation>Aby logowanie zadziałało, musisz ponownie uruchomić aplikację.</translation>
+    </message>
+    <message>
+        <source>This will disable log collection, you should restart the app afterwards so it takes full effect.</source>
+        <extracomment>Full effect here means that the setting will not propagate to all parts of the app so some parts will still collect logs</extracomment>
+        <translation>Ta opcja wyłączy zbieranie logów. Należy ponownie uruchomić aplikację, aby wyłączenie zadziałało.</translation>
+    </message>
+    <message>
+        <source>After disabling logging all previous logs will be deleted to save space.</source>
+        <translation>Po wyłączeniu logowania wszystkie poprzednie logi zostaną usunięte, aby zaoszczędzić miejsce.</translation>
+    </message>
+    <message>
+        <source>If you have not exported the logs anywhere you should do so before disabling logging.</source>
+        <translation>Jeśli nigdzie nie wyeksportowałeś logów, powinieneś to zrobić przed wyłączeniem logowania.</translation>
+    </message>
+    <message>
+        <source>Custom system message</source>
+        <translation>Niestandardowa wiadomość systemowa</translation>
+    </message>
+    <message>
+        <source>You can set a system message that is sent as an instruction to GPT before each chat begins.</source>
+        <translation>Możesz ustawić wiadomość systemową, która będzie wysyłana jako instrukcja do GPT przed rozpoczęciem każdego czatu.</translation>
+    </message>
+    <message>
+        <source>Note: The current version of GPT doesn&apos;t pay strong attention to the system message, it should be mitigated when the model is updated.</source>
+        <translation>Uwaga: obecna wersja GPT nie przywiązuje dużej wagi do komunikatu systemowego, należy go złagodzić, gdy model zostanie zaktualizowany.</translation>
+    </message>
+    <message>
+        <source>Note: The system message counts towards your &apos;%1&apos; limit.</source>
+        <extracomment>The %1 gets replaced by the translation for 'Conversation length'</extracomment>
+        <translation>Uwaga: wiadomość systemowa wlicza się do limitu &quot;%1&quot; wiadomości.</translation>
+    </message>
+    <message>
+        <source>Conversation length</source>
+        <translation>Długość rozmowy</translation>
+    </message>
+    <message>
+        <source>For example you can write &apos;You are a helpful assistant&apos; and the AI should try to follow your instructions.</source>
+        <translation>Na przykład możesz napisać &quot;Jesteś pomocnym asystentem&quot;, a sztuczna inteligencja powinna starać się postępować zgodnie z twoimi instrukcjami.</translation>
+    </message>
+</context>
+</TS>
